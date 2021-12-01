69817ad4b816e53d28817794426a94ecaaa0352f
Elizaveta Zazhigina <43406459+snumbrikvolgo@users.noreply.github.com>
2021-12-02 01:09:52 +0300

    return all mapping function
    allows not to add several times chargers
    
    also proper button handler

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,6 @@ App = {
   contracts: {},
 
   init: async function() {
-    // Load chargers.
     $.getJSON('../chargers.json', function(data) {
       var chargersRow = $('#chargersRow');
       var chargerTemplate = $('#chargerTemplate');
@@ -24,6 +23,9 @@ App = {
         chargerTemplate.find('.charge-longitude').text(data[i].longitude);
         chargerTemplate.find('.charge-oracleAddress').text(data[i].oracleAddress)
         chargerTemplate.find('.btn-charge').attr('data-id', data[i].id);
+        chargerTemplate.find('calendar1').attr('name', data[i].id);
+        chargerTemplate.find('calendar2').attr('name', data[i].id);
+
 
         chargersRow.append(chargerTemplate.html());
       }
@@ -42,11 +44,9 @@ App = {
         console.error("User denied account access")
       }
     }
-    // Legacy dapp browsers...
     else if (window.web3) {
       App.web3Provider = window.web3.currentProvider;
     }
-    // If no injected web3 instance is detected, fall back to Ganache
     else {
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
     }
@@ -56,39 +56,31 @@ App = {
   },
 
   initContract: function() {
-    document.getElementById("Res").innerHTML = "InitContract"
     $.getJSON('ChargersListing.json', function(data) {
-      // Get the necessary contract artifact file and instantiate it with truffle-contract
       var ChargersListArtifact = data;
       App.contracts.ChargersListing = TruffleContract(ChargersListArtifact);
-      console.log(142);
-      // Set the provider for our contract
       App.contracts.ChargersListing.setProvider(App.web3Provider);
       web3.eth.getAccounts(function(error, accounts) {
         if (error) {
           console.log(error);
         }
         var account = accounts[0];
-        // $.getJSON('/home/snumbrikvolgo/ChargerProject/build/contracts/Charger.json', function(Charger) {
-        //   console.log('hui')
-        // });
-        console.log(accounts);
         App.contracts.ChargersListing.deployed().then(function(instance) {
           var chargingInstance
           chargingInstance = instance;
-          $.getJSON('../chargers.json', function(data) {
-            for (i = 0; i < data.length; i ++) {
-              if (chargingInstance.chargers(i) == '0x0000000000000000000000000000000000000000')
+          chargingInstance.getAllChargers().then(function(chargers){
+            for (let i = 0; i < chargers.length; i ++){
+              if (chargers[i] == '0x0000000000000000000000000000000000000000')
               {
-              chargingInstance.addCharger(data[i].id, data[i].power, data[i].cableType, data[i].tariff, data[i].latitude, data[i].longitude, data[i].oracleAddress, {from: account})
-              console.log(chargingInstance.getChargerInfo(i));
+                $.getJSON('../chargers.json', function(data) {
+                    console.log(data[i])
+                    chargingInstance.addCharger(data[i].id, data[i].power, data[i].cableType, data[i].tariff, data[i].latitude, data[i].longitude, data[i].oracleAddress, {from: account})       
+                });
               }
-
             }
           });
         });
       });
-    
       return App.markCharging();
     });
 
@@ -102,20 +94,17 @@ App = {
   markCharging: function(ChargersIndexes, account) {
     var chargingInstance;
     App.contracts.ChargersListing.deployed().then(function(instance) {
-      document.getElementById("Res").innerHTML = "markCharging";
       chargingInstance = instance;
-
       return chargingInstance.getAllChargersIndexes();
     }).then(function(ChargersIndexes) {
       console.log(ChargersIndexes)
-      for (i = 0; i < ChargersIndexes.length; i++) { 
-        if (chargingInstance.chargers(i) !== '0x0000000000000000000000000000000000000000') {
-          console.log(chargingInstance.chargers(i))
-
-          $('.panel-chargers').eq(i).find('button').text('Success').attr('disabled', false);
-        }
+      for (let i = 0; i < ChargersIndexes.length; i++) {
+        chargingInstance.chargers(i).then(function(ch){
+          if (ch == '0x0000000000000000000000000000000000000000') {
+            $('.panel-chargers').eq(i).find('button').text('Submit').attr('disabled', true);
+          }
+        });
       }
-      // return true;
     }).catch(function(err) {
       console.log(err.message);
     });
@@ -123,54 +112,44 @@ App = {
 
   handleCharging: function(event) {
     event.preventDefault();
-    
-    document.getElementById("ResultBad").innerHTML = null;
-    var beginTime = document.getElementById("appt1").value;
+    var chargeId = parseInt($(event.target).data('id'));
+    document.getElementsByClassName('bad').item(chargeId).innerHTML = "";
+    document.getElementsByClassName("good").item(chargeId).innerHTML = "";
+    var beginTime = document.getElementsByClassName('calendar1').item(chargeId).value;
+    console.log(chargeId);
+
     console.log(beginTime);
-    var endTime = document.getElementById("appt2").value;
+    var endTime = document.getElementsByClassName('calendar2').item(chargeId).value;
     console.log(endTime);
 
     var beginMinutes = (Date.parse(beginTime))/1000/300;
     var diffMinutes = (Date.parse(endTime) - Date.parse(beginTime))/1000/300;
-    document.getElementById("Res").innerHTML = "HI"
     if(beginTime && endTime){
       if(diffMinutes<=0)
       {
-        document.getElementById("ResultBad").innerHTML = "Contract wasn't made begin time more then end time";
+        document.getElementsByClassName('bad').item(chargeId).innerHTML = "Contract wasn't made begin time more then end time";
         return false;
       }
     }
     else {
-      document.getElementById("ResultBad").innerHTML = "Contract wasn't made not both times were passed";
+      document.getElementsByClassName('bad').item(chargeId).innerHTML = "Contract wasn't made begin time more then end time";
       return false;
     }
-    document.getElementById("Res").innerHTML = "HI after if";
-    var chargeId = parseInt($(event.target).data('id'));
-
+    // var chargeId = parseInt($(event.target).data('id'));
     var chargingInstance;
-
     web3.eth.getAccounts(function(error, accounts) {
       if (error) {
         console.log(error);
       }
       var account = accounts[0];
       console.log(accounts[0]);
-      document.getElementById("Res").innerHTML = "HI before deploy";
       App.contracts.ChargersListing.deployed().then(function(instance) {
         chargingInstance = instance;
-
-        // Execute adopt as a transaction by sending account
-        document.getElementById("Res").innerHTML = "Before deposit";
-        // var p = chargingInstance.getAllChargersIndexes();
-        console.log(chargingInstance.getCharger(chargeId));
-        document.getElementById("Res").innerHTML = "Got all chargers indexes";
-        // var Charger = require('../../build/contracts/Charger.json')
-        // const fs = require('../../node_modules/fs/package.json');
-        // const contract = JSON.parse(fs.readFileSync('../../build/contracts/Charger.json', 'utf8'));
-        // var targetCharger = new web3.eth.Contract(contract.abi, chargingInstance.chargers(chargeId))
         return chargingInstance.getChargerRegisterDeposit(chargeId, beginMinutes,diffMinutes,0, {from: account});
       }).then(function(result) {
-        console.log('hui')
+        console.log('depositOK')
+        document.getElementsByClassName('bad').item(chargeId).innerHTML = "";
+        document.getElementsByClassName("good").item(chargeId).innerHTML = "Successfully Registered";
         return App.markCharging();
       }).catch(function(err) {
         console.log(err.message);
